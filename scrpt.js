d09a8e286cc521d843acedf74a76562c2bc2f392
MatTeo-BT <aboutmet@gmail.com>
2023-11-15 18:03:02 +0100

    modidifca eserizio dadi

--- scrpt.js
+++ scrpt.js
@@ -4,11 +4,13 @@ const gamerNumber= Math.floor(Math.random() * 7);
 
 if(AiNumber > gamerNumber){
     console.log(`CPU WIN`)
-    document.getElementById("casual-computer-number").innerHTML = AiNumber;
+   
 } else if(AiNumber < gamerNumber){
     console.log(`GAMER WIN`)
-    document.getElementById("casual-gamer-number").innerHTML = gamerNumber;
+    
 }
+document.getElementById("casual-computer-number").innerHTML = AiNumber;
+document.getElementById("casual-gamer-number").innerHTML = gamerNumber;
 
 
 
